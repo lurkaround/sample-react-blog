26079e85c06cedbe17beef070a360b2c1ac8c96a
Helon <rhelon@gmail.com>
2019-08-08 09:57:08 +0200

    Added sample articles data and routed displays

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
 } from 'react-router-dom';
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage';
-import ArticlesList from './pages/ArticlesList';
+import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from "./pages/ArticlePage";
 import NavBar from './NavBar';
 import './App.css';
@@ -19,8 +19,8 @@ class App extends Component {
           <div id='page-body'>
             <Route path='/' component={HomePage} exact />
             <Route path='/about' component={AboutPage} />
-            <Route path='/articles-list' component={ArticlesList} />
-            <Route path='/article' component={ArticlePage} />
+            <Route path='/articles-list' component={ArticlesListPage} />
+            <Route path='/article/:name' component={ArticlePage} />
           </div>
         </div>
       </Router>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,7 +1,25 @@
 import React from "react";
+import ArticlesList from '../components/ArticlesList';
+import articleContent from './article-content';
 
-const ArticlePage = () => (
-    <h1>This is an Article</h1>
-);
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = articleContent.find(article => article.name === name);
+
+    if (!article) return <h1>Article doesn't exist</h1>
+
+    const otherArticles = articleContent.filter(article => article.name !== name)
+
+    return (
+        <>
+        <h1>{article.title}</h1>
+        {article.content.map((paragraph, key) => (
+            <p key={key}>{paragraph}</p>
+        ))}
+        <h3>Other Articles: </h3>
+        <ArticlesList articles={otherArticles} />
+        </>
+    );
+}
 
 export default ArticlePage;
