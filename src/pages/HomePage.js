4ad530cceda85ac0714d8531d9d0f03649aa7086
Helon <rhelon@gmail.com>
2019-08-08 08:59:24 +0200

    initial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+      <h1>Hello, welcome to my blog!</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. A quos id
+        illo ullam, sint officia harum amet sequi eveniet quae distinctio!
+        Iure molestiae aut quod ipsum voluptatem esse culpa doloribus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        debitis distinctio dolorem beatae ipsa necessitatibus omnis?
+        Consequuntur unde fugiat repellendus, porro aperiam, recusandae
+        laboriosam hic iusto ab iure amet quisquam?
+      </p>
+      </>
+)
+
+export default HomePage;
